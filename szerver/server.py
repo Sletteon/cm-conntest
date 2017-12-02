77b4a8a44959b45f6b6e6ee21ef262171eeab1dd
sletteon <Sletteon@users.noreply.github.com>
2017-12-02 21:56:39 +0100

    indításnál importolunk minden osztályt (travis)

--- szerver/server.py
+++ szerver/server.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from libserver import PyWSock
+from exlibserver import PyWSockFunc
 import sys
 try:
 	port = 5000
@@ -7,6 +8,7 @@ try:
 		print("Port: 5000")
 	if len(sys.argv) == 2:
 		if sys.argv[1] == "travis":
+			print("<+> Minden jónak tűnik az osztályok felől...")
 			exit()
 		port = int(sys.argv[1])
 	ws = PyWSock(port)
