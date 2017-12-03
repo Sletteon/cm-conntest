d7bf6b044842ada55588ed063c1587867cc33988
sletteon <misi.g126@gmail.com>
2017-12-03 20:28:06 +0100

    szépítés, fölös kommentek kiszedése

--- szerver/exlibserver.py
+++ szerver/exlibserver.py
@@ -1,126 +1,128 @@
 # -*- coding: utf-8 -*-
-import socket, hashlib, base64, threading, time
+import socket
+import hashlib
+import base64
+import threading
+import time
 # =======
 # Ez az osztály kiegészíti a PyWSock osztályt ami a libservet.py fileban található
 # =======
+
+
 class PyWSockFunc:
-	# networking stringek
-	MAGIC = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-	HSHAKE_RESP = "HTTP/1.1 101 Switching Protocols\r\n" + \
-				"Upgrade: websocket\r\n" + \
-				"Connection: Upgrade\r\n" + \
-				"Sec-WebSocket-Accept: %s\r\n" + \
-				"\r\n"
-	# handshakelés, ne írja ki a választ, csak annyit, hogy sikeres volt-e
-	# egyrészt a http követeli meg a handshaket, de még a kapcsolat tesztelésére is jó
-	def handshake (self, client, addrh):
-		# print('<-> Handshake: ' + addrh[0])
-		data = client.recv(2048)
-		headers = self.parse_headers(data)
-		# print('<+> Handshake sikeres')
-		key = headers['Sec-WebSocket-Key']
-		resp_data = self.HSHAKE_RESP % ((base64.b64encode(hashlib.sha1(key+self.MAGIC).digest()),))
-		#print('[%s]' % (resp_data,))
-		return client.send(resp_data)
+    MAGIC = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+    HSHAKE_RESP = "HTTP/1.1 101 Switching Protocols\r\n" + \
+        "Upgrade: websocket\r\n" + \
+        "Connection: Upgrade\r\n" + \
+        "Sec-WebSocket-Accept: %s\r\n" + \
+        "\r\n"
 
-	# adatok befogadásához egy metódus
-	# na ezt a metódust sem értem
-	def recv_data (self, client):
-		# as a simple server, we expect to receive:
-		#    - all data at one go and one frame
-		#    - one frame at a time
-		#    - text protocol
-		#    - no ping pong messages
-		data = bytearray(client.recv(512))
-		if(len(data) < 6):
-			raise Exception("Adatolvasás hiba")
-		# FIN bit must be set to indicate end of frame
-		assert(0x1 == (0xFF & data[0]) >> 7)
-		# data must be a text frame
-		# 0x8 (close connection) is handled with assertion failure
-		assert(0x1 == (0xF & data[0]))
-		# assert that data is masked
-		assert(0x1 == (0xFF & data[1]) >> 7)
-		datalen = (0x7F & data[1])
-		#print("received data len %d" %(datalen,))
-		str_data = ''
-		if(datalen > 0):
-			mask_key = data[2:6]
-			masked_data = data[6:(6+datalen)]
-			unmasked_data = [masked_data[i] ^ mask_key[i%4] for i in range(len(masked_data))]
-			str_data = str(bytearray(unmasked_data))
-		return str_data
-
-		# broadcast = mindenkinek üzenetet kiküldeni
-		# lényegében egy tuningolt client.send
-	def broadcast(self, data):
-		# 1st byte: fin bit set. text frame bits set.
-		# 2nd byte: no mask. length set in 1 byte.
-		resp = bytearray([0b10000001, len(data)])
-		# append the data bytes
-		for d in bytearray(data):
-			resp.append(d)
-		self.LOCK.acquire()
-		for client in self.clients:
-			try:
-				client.send(resp)
-			except Exception as e:
-				print("<!> Broadcast hiba:" + e)
-		self.LOCK.release()
-
-	# formázás (szintén nem értem)
-	def parse_headers (self, data):
-		headers = {}
-		lines = data.splitlines()
-		for l in lines:
-			parts = l.split(": ", 1)
-			if len(parts) == 2:
+    def parseHeaders(self, data):
+    	headers = {}
+    	lines = data.splitlines()
+    	for l in lines:
+        	parts = l.split(": ", 1)
+        	if len(parts) == 2:
 				headers[parts[0]] = parts[1]
 		headers['code'] = lines[len(lines) - 1]
-		return headers
+    	return headers
 
-	# listát sztringekbe konvertálja, és így olvassa be a klienseknek
-	def szepitveBeolvas(self, xfiletart):
-		# létrehoz egy stringet az xfiletart listábol
-		xfiletartstr = "".join(str(x) for x in xfiletart)
-		# ha szóköz nélküli adat nem üres, küldje azt el
-		if xfiletartstr.replace(" ","") != "":
-			self.broadcast(xfiletartstr)
-			# egyébként sokszor utálom, de most jól jön
-			# egy üres line ez a print után,
-			# ugyanis mi hármasával küldjük el az xfiletartstr-t,
-			# és nagyon szépen elválasztja 3-asával a küldött parancsokat
-			print(xfiletartstr)
+    # egyrészt a http követeli meg a handshaket, de még a kapcsolat tesztelésére is jó
+    def handshake(self, client, addrh):
+        # print('<-> Handshake: ' + addrh[0])
+        data = client.recv(2048)
+        headers = self.parseHeaders(data)
+        # print('<+> Handshake sikeres')
+        key = headers['Sec-WebSocket-Key']
+        responseData = self.HSHAKE_RESP % (
+            (base64.b64encode(hashlib.sha1(key + self.MAGIC).digest()),))
+        #print('[%s]' % (responseData,))
+        return client.send(responseData)
 
-	# csak elmenti a megadott fájlba a megadott adatot,
-	# mivel kétszer kellett ugyanazt írnom, gondoltam,
-	# talán egyszerübben fut, ha külön metódusba írom
-	def ment(self, xfile, data):
-		xfile.write(data + '\n')
+    def receiveData(self, client):
+        # as a simple server, we expect to receive:
+        #    - all data at one go and one frame
+        #    - one frame at a time
+        #    - text protocol
+        #    - no ping pong messages
+        data = bytearray(client.recv(512))
+        if(len(data) < 6):
+            raise Exception("Adatolvasás hiba")
+        # FIN bit must be set to indicate end of frame
+        assert(0x1 == (0xFF & data[0]) >> 7)
+        # data must be a text frame
+        # 0x8 (close connection) is handled with assertion failure
+        assert(0x1 == (0xF & data[0]))
+        # assert that data is masked
+        assert(0x1 == (0xFF & data[1]) >> 7)
+        datalen = (0x7F & data[1])
+        #print("received data len %d" %(datalen,))
+        dataString = ''
+        if(datalen > 0):
+            maskKey = data[2:6]
+            maskedData = data[6:(6 + datalen)]
+            unmaskedData = [maskedData[i] ^ maskKey[i % 4]
+                            for i in range(len(maskedData))]
+            dataString = str(bytearray(unmaskedData))
+        return dataString
 
-	# fájlok tartalmának törlése, ha nincs meg a fájl,
-	# hozza létre azt.
-	# Ha felhasználóknak adjuk, TÖRÖLNI kell
-	# ha mégse, a beírt parancsok csak a szerver futásáig
-	# maradnak meg
-	def filetrunc(self):
-		efile = open("debug/E.ssv","a+")
-		efile.truncate()
-		efile.close()
+    def broadcast(self, data):
+        # 1st byte: fin bit set. text frame bits set.
+        # 2nd byte: no mask. length set in 1 byte.
+        resp = bytearray([0b10000001, len(data)])
+        # append the data bytes
+        for d in bytearray(data):
+            resp.append(d)
+        self.LOCK.acquire()
+        for client in self.clients:
+            try:
+                client.send(resp)
+            except Exception as e:
+                print("<!> Broadcast hiba:" + e)
+        self.LOCK.release()
 
-		jfile = open("debug/J.ssv", "a+")
-		jfile.truncate()
-		jfile.close()
+    # listát sztringekbe konvertálja, és így olvassa be a klienseknek
+    def szepitveBeolvas(self, xfiletart):
+        # létrehoz egy stringet az xfiletart listábol
+        xfiletartstr = "".join(str(x) for x in xfiletart)
+        # ha szóköz nélküli adat nem üres, küldje azt el
+        if xfiletartstr.replace(" ", "") != "":
+            self.broadcast(xfiletartstr)
+            # egyébként sokszor utálom, de most jól jön
+            # egy üres line ez a print után,
+            # ugyanis mi hármasával küldjük el az xfiletartstr-t,
+            # és nagyon szépen elválasztja 3-asával a küldött parancsokat
+            print(xfiletartstr)
 
-	# 3-asával elküldi a parancsokat (szépítve)
-	def listaTobbreOsztvaElkuld(self, lista):
-		if len(lista) >= 3:
-			x = 0
-			y = 3
-			self.szepitveBeolvas(lista[x:y])
-			for i in range(len(lista) - 3):
-				x += 3
-				y += 3
-				self.szepitveBeolvas(lista[x:y])
-			if len(lista) == 1:
-				self.szepitveBeolvas(lista)
+    # csak elmenti a megadott fájlba a megadott adatot,
+    # mivel kétszer kellett ugyanazt írnom, gondoltam,
+    # talán egyszerübben fut, ha külön metódusba írom
+    def ment(self, xfile, data):
+        xfile.write(data + '\n')
+
+    # fájlok tartalmának törlése, ha nincs meg a fájl,
+    # hozza létre azt.
+    # Ha felhasználóknak adjuk, TÖRÖLNI kell
+    # ha mégse, a beírt parancsok csak a szerver futásáig
+    # maradnak meg
+    def filetrunc(self):
+        efile = open("debug/E.ssv", "a+")
+        efile.truncate()
+        efile.close()
+
+        jfile = open("debug/J.ssv", "a+")
+        jfile.truncate()
+        jfile.close()
+
+    # 3-asával elküldi a parancsokat (szépítve)
+    def listatKuld(self, lista):
+        if len(lista) >= 3:
+            x = 0
+            y = 3
+            self.szepitveBeolvas(lista[x:y])
+            for i in range(len(lista) - 3):
+                x += 3
+                y += 3
+                self.szepitveBeolvas(lista[x:y])
+            if len(lista) == 1:
+                self.szepitveBeolvas(lista)
--- szerver/libserver.py
+++ szerver/libserver.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # importok
-import threading, time, socket
+import threading
+import time
+import socket
 from exlibserver import PyWSockFunc
 # hatalmas segítség: https://stackoverflow.com/questions/18240358/html5-websocket-connecting-to-python
 
@@ -17,123 +19,113 @@ from exlibserver import PyWSockFunc
 # 		A szerver.pyban meghívjuk egy objekummal a PyWSock osztálynak
 #		a __init__ metódusát.
 # 		Ez a metódus a beérkező kapcsolatokat fogadja el,
-# 		minden kliensnek fusson le a handle_client, külön-külön szálakon.
+# 		minden kliensnek fusson le a handleClient, külön-külön szálakon.
 # 		Az exlibserver.py fájlban a klienseknek futásához szükséges
 #		globális változókat, illetve további metódusokat találjuk meg, amik
-# 		többnyire a handle_client funkcióban kerülnek felhasználásra,
+# 		többnyire a handleClient funkcióban kerülnek felhasználásra,
 # 		illetve meghívásra.
 # 		A szervert tehát 2 while(True) ciklus hajtja: egy figyelő, illetve egy
 # 		kliens-kiszolgáló.
 # 		Az utóbbiban a loop a kliensek parancsat figyeli, illetve azokra reagál.
 # =====================================
 
+
 class PyWSock(PyWSockFunc):
-	LOCK = threading.Lock()
-	# kliens lista deklarálása
-	clients = []
+    LOCK = threading.Lock()
+    clients = []
 
-	# mindent elindít
-	def __init__(self, port):
-		self.filetrunc()
-		# socket cuccok
-		s = socket.socket()
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		s.bind(('', port))
-		# maximum 50 kapcsolatot fogadjon el
-		# lehetne sokkal kevesebb is, mert a kapcsolat nem keep-alive,
-		# a kliensek kevés idő erejéig kapcsolódnak a szerverhez,
-		# legalábbis ezt tervezem
-		s.listen(50)
-		print ('<+> Szerver online')
-		while(1):
-			# fogadjon el minden beérkező kapcsolatot
-			conn, addr = s.accept()
-			# írja ki szépen a kliensek IP-címét,
-			# jó sok plusszal, hogy látszódjon, ki kapcsolódott
-			print ('+++' + addr[0] + '+++')
-			# threadingesen hívja meg a handle_client metódust
-			# gondolom külön-külön szálakat foglaljon le a handle_client metódusnak
-			threading.Thread(target = self.handle_client, args = (conn, addr)).start()
-			# threading cuccok, kliensek objektumát írja a
-			# kliens lista végére.
-			self.LOCK.acquire()
-			self.clients.append(conn)
-			self.LOCK.release()
-			time.sleep(0.1)
+    def __init__(self, port):
+        self.filetrunc()
+        # socket cuccok
+        s = socket.socket()
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('', port))
+        # maximum 50 kapcsolatot fogadjon el
+        # lehetne sokkal kevesebb is, mert a kapcsolat nem keep-alive,
+        # a kliensek kevés idő erejéig kapcsolódnak a szerverhez,
+        # legalábbis ezt tervezem
+        s.listen(50)
+        print('<+> Szerver online')
+        while 1:
+            conn, addr = s.accept()
+            # írja ki szépen a kliensek IP-címét,
+            # jó sok plusszal, hogy látszódjon, ki kapcsolódott
+            print('+++' + addr[0] + '+++')
+            # threadingesen hívja meg a handleClient metódust
+            # gondolom külön-külön szálakat foglaljon le a handleClient metódusnak
+            threading.Thread(target=self.handleClient,
+                             args=(conn, addr)).start()
+            # threading cuccok, kliensek objektumát írja a
+            # kliens lista végére.
+            self.LOCK.acquire()
+            self.clients.append(conn)
+            self.LOCK.release()
+            time.sleep(0.1)
 
-	# mit csináljon a kliensekkel (egyenként)
-	def handle_client (self, client, addr):
-		# handshake
-		self.handshake(client, addr)
-		# nyissa meg az E.db fájlt, írás-olvasás joggal
-		efile = open("debug/E.ssv","a+")
-		jfile = open("debug/J.ssv","a+")
+    # mit csináljon a kliensekkel (egyenként)
+    def handleClient(self, client, addr):
+        self.handshake(client, addr)
+        # nyissa meg a fájlokat, írás-olvasás joggal
+        EFile = open("debug/E.ssv", "a+")
+        JFile = open("debug/J.ssv", "a+")
 
-		try:
-			while 1:
-				# Adat elkapása
-				data = self.recv_data(client)
-				print("Parancs: " + data)
+        try:
+            while 1:
+                # Adat elkapása
+                data = self.receiveData(client)
+                print("Parancs: " + data)
 
-				# hozza létre az efiletart változót,
-				# és mindig frissítse, így mindenki a legfrissebb
-				# változatot kapja
-				# efiletart: [e]z a hét [file]-jának [tart]alma
-				efiletart = efile.readlines()
-				jfiletart = jfile.readlines()
+                # hozza létre az EFileTart változót,
+                # és mindig frissítse, így mindenki a legfrissebb
+                # változatot kapja
+                # EFileTart: [e]z a hét [file]-jának [tart]alma
+                EFileTart = EFile.readlines()
+                JFileTart = JFile.readlines()
 
-				# a parancsok stringek, pontosvesszővel elválasztva,
-				# itt kerülnek szétbontásra, egy splitdata listába
-				splitdata = data.split(';')
-				# mindig írja ki a kapott adattól, kitől kaptuk azt
-				print ('Felhasználónév: ' + splitdata[0])
+                splitdata = data.split(';')
+                print('Felhasználónév: ' + splitdata[0])
 
-				# ha valaki küldött egy set parancsot
-				if splitdata[1] == 'set':
+                if splitdata[1] == 'set':
 
-					print("Beállítás")
+                    print("Beállítás")
 
-					if splitdata[2] == 'E':
-						self.ment(efile, data)
-					else:
-						if splitdata[2] == 'J':
-						   self.ment(jfile, data)
+                    if splitdata[2] == 'E':
+                        self.ment(EFile, data)
+                    else:
+                        if splitdata[2] == 'J':
+                            self.ment(JFile, data)
 
-					print ('Nap: ' + splitdata[3])
-					print ('Tantárgy: ' + splitdata[4])
-					print ('Anyag: ' + splitdata[5])
+                    print('Nap: ' + splitdata[3])
+                    print('Tantárgy: ' + splitdata[4])
+                    print('Anyag: ' + splitdata[5])
 
-				else:
-					print("Lekérés")
-					# ha valaki le szeretné kérni ennek a hétnek az anyagát, küldje is el
-					if splitdata[2] == 'E':
-						print('Hét: Ez a hét')
-						self.listaTobbreOsztvaElkuld(efiletart)
-					# jövő hét anyaga, ugyanaz, mint az e heti
-					if splitdata[2] == 'J':
-						print('Hét: Jövő hét')
-						self.listaTobbreOsztvaElkuld(jfiletart)
-				# szabályszerűen zárja le a fáljt
-				efile.close()
-				# ne egye meg a CPU-t
-				time.sleep(0.1)
+                else:
+                    print("Lekérés")
+                    if splitdata[2] == 'E':
+                        print('Hét: Ez a hét')
+                        self.listatKuld(EFileTart)
+                    if splitdata[2] == 'J':
+                        print('Hét: Jövő hét')
+                        self.listatKuld(JFileTart)
+                EFile.close()
+                # ne egye meg a CPU-t
+                time.sleep(0.1)
 
-		except Exception as e:
-			# ha valami hiba történt, írja ki
-			# debughoz hasznos, de gyakran kiírja,
-			# hogy adatolvasási hiba
-			# és mindig beilleszt egy kéretlen newlinet,
-			# hacsak nem ír ki valamit
-			print(e)
-			# ha nem írunk ki semmit, ne történjen semmi
-			# ha tényleg nincs szükségünk az elöbbi printre,
-			# ki lehet a try-t törölni
-			pass
-		# mindenesetben, ha kiléptünk a while loopból,
-		# 100% hogy egy kliens lecsatlakozott, írja ki, hogy ki
-		print ('---' + addr[0] + '---\n')
-		# threading, amihez megint csak keveset értek
-		self.LOCK.acquire()
-		self.clients.remove(client)
-		self.LOCK.release()
-		client.close()
+        except Exception as e:
+            # ha valami hiba történt, írja ki
+            # debughoz hasznos, de gyakran kiírja,
+            # hogy adatolvasási hiba
+            # és mindig beilleszt egy kéretlen newlinet,
+            # hacsak nem ír ki valamit
+            print(e)
+            # ha nem írunk ki semmit, ne történjen semmi
+            # ha tényleg nincs szükségünk az elöbbi printre,
+            # ki lehet a try-t törölni
+            pass
+        # mindenesetben, ha kiléptünk a while loopból,
+        # 100% hogy egy kliens lecsatlakozott
+        print('---' + addr[0] + '---\n')
+        self.LOCK.acquire()
+        self.clients.remove(client)
+        self.LOCK.release()
+        client.close()
