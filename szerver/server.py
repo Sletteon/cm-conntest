12795b8779938f7e21229bb2851f30a16ed4f1f5
sletteon <Sletteon@users.noreply.github.com>
2017-12-15 19:46:17 +0100

    további szépítések

--- szerver/server.py
+++ szerver/server.py
@@ -3,12 +3,11 @@ import os, logging
 from flask import Flask, request, Response, json
 from flask_cors import CORS
 
-# Ne látszódjanak a werkzeug cuccai
+# Ne látszódjanak a werkzeug (többek között HTTP-log) cuccai
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 STATIC_FOLDER = os.path.join(os.environ['PWD'], 'client')
-# app objektum
 app = Flask(__name__, static_folder = STATIC_FOLDER)
 # Kell, ha nincs akkor kliens hibát kap
 CORS(app, resources={r'/*': {'origins': '*'}})
@@ -29,11 +28,11 @@ def filetrunc():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	IP = request.remote_addr
+	clientIP = request.remote_addr
 	if request.method == 'POST':
 		gotJSON = request.get_json()
-		# [*] jancsi (jancsi.ip.kakaós.csiga) bejegyzése:
-		print('\n' + '[*] ' + str(gotJSON['uname']) + ' (' + IP + ')' + ' bejegyzése: ')
+		# [*] jancsi (jancsi.ip.címe.briós) bejegyzése:
+		print('\n' + '[*] ' + str(gotJSON['uname']) + ' (' + clientIP + ')' + ' bejegyzése: ')
 		# próbálja meg a beküldött adatot (JSON) szétválasztani,
 		# lokális változókat létrehozva
 		try:
@@ -61,8 +60,8 @@ def index():
 				jfile.write(separatorChar.join([uname, het, nap, tant, anyag]) + '\n')
 		return Response(json.dumps('SUCCESS'), mimetype='application/json')
 	else: #request.method == 'GET'
-		# [*] Hétlekérés: jancsi.ip.lekváros.kenyér
-		print('\n' + '[*] Hétlekérés: ' + IP)
+		# [*] Hétlekérés: jancsi.ip.címe.túróstáska
+		print('\n' + '[*] Hétlekérés: ' + clientIP)
 
 		# Átmeneti (szörnyű) megoldás: hozzon létre változókat,
 		# ha nincs pl. a jövő heti fájlba írva semmi,
@@ -72,12 +71,13 @@ def index():
 		with open('debug/E.ssv', 'a+') as efile:
 			line = efile.readline()
 			if line != '' and separatorChar in line:
-				unameE, hetE, napE, tantE, anyagE = line.strip().split(separatorChar)
+					unameE, hetE, napE, tantE, anyagE = line.strip().split(separatorChar)
 
 		with open('debug/J.ssv', 'a+') as jfile:
 			line = jfile.readline()
 			if line != '' and separatorChar in line:
-				unameJ, hetJ, napJ, tantJ, anyagJ = line.strip().split(separatorChar)
+					unameJ, hetJ, napJ, tantJ, anyagJ = line.strip().split(separatorChar)
+
 
 		returnJSON = {
 			'E':
@@ -101,8 +101,9 @@ def index():
 		return Response(json.dumps(returnJSON))
 
 if __name__ == '__main__':
-	# Ha bennhagyjuk, leállításig rögzített bejegyzések úrjaindításkor törlődnek
+	# Ha bennhagyjuk, a rögzített bejegyzések úrjaindításkor törlődnek
 	filetrunc();
+
+	app.run(host='0.0.0.0')
 	# Pozitívum, ha már itt tartunk
 	print('[+] Szerver fut')
-	app.run(host='0.0.0.0')
