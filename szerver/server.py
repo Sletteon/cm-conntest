bcb4d14b9e240a77774bd21ff04a1388eba6aa61
sletteon <Sletteon@users.noreply.github.com>
2017-12-15 17:45:54 +0100

    dokumentáció

--- szerver/server.py
+++ szerver/server.py
@@ -3,16 +3,20 @@ import os, logging
 from flask import Flask, request, Response, json
 from flask_cors import CORS
 
+# Ne látszódjanak a werkzeug cuccai
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 STATIC_FOLDER = os.path.join(os.environ['PWD'], 'client')
+# app objektum
 app = Flask(__name__, static_folder = STATIC_FOLDER)
+# Kell, ha nincs akkor kliens hibát kap
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-SEPARATOR = '<|>'
+# Ezzel választja el a fájlokban az adatokat
+separatorChar = '<|>'
 
-
+# Töröjle a megnyitott fájlok tartalmát
 def filetrunc():
 	with open('debug/E.ssv', 'a+') as efile:
 		efile.truncate()
@@ -28,7 +32,10 @@ def index():
 	IP = request.remote_addr
 	if request.method == 'POST':
 		gotJSON = request.get_json()
-		print('\n' + '[+] ' + str(gotJSON['uname']) + ' (' + IP + ')' + ' bejegyzése: ')
+		# [*] jancsi (jancsi.ip.kakaós.csiga) bejegyzése:
+		print('\n' + '[*] ' + str(gotJSON['uname']) + ' (' + IP + ')' + ' bejegyzése: ')
+		# próbálja meg a beküldött adatot (JSON) szétválasztani,
+		# lokális változókat létrehozva
 		try:
 			uname = gotJSON['uname']
 			het = gotJSON['het']
@@ -38,29 +45,39 @@ def index():
 		except KeyError:
 			response = json.dumps({'error': 'Missing input parameter'})
 			return Response(response, status=422, mimetype='application/json')
+
+		# Adatok kiírása
 		print('--- Hét: ' + str(het))
 		print('--- Nap: ' + str(nap))
 		print('--- Tantárgy: ' + str(tant))
 		print('--- Anyag: ' + str(anyag))
+
+		# Adatok mentése a megfelelő fájlba
 		if het == 'E':
 			with open('debug/E.ssv', 'a+') as efile:
-				efile.write(SEPARATOR.join([uname, het, nap, tant, anyag]) + '\n')
+				efile.write(separatorChar.join([uname, het, nap, tant, anyag]) + '\n')
 		if het == 'J':
 			with open('debug/J.ssv', 'a+') as jfile:
-				jfile.write(SEPARATOR.join([uname, het, nap, tant, anyag]) + '\n')
+				jfile.write(separatorChar.join([uname, het, nap, tant, anyag]) + '\n')
 		return Response(json.dumps('SUCCESS'), mimetype='application/json')
 	else: #request.method == 'GET'
-		print('\n' + '[+] Hétlekérés: ' + IP)
+		# [*] Hétlekérés: jancsi.ip.lekváros.kenyér
+		print('\n' + '[*] Hétlekérés: ' + IP)
+
+		# Átmeneti (szörnyű) megoldás: hozzon létre változókat,
+		# ha nincs pl. a jövő heti fájlba írva semmi,
+		# akkor is küldje el a jövő heti fájl kulcsait, '' értékkel
 		unameE, hetE, napE, tantE, anyagE, unameJ, hetJ, napJ, tantJ, anyagJ = '' , '' , '' , '' , '' , '' , '' , '' , '' , ''
+		# Nyissa meg a fájlokat, írja a memóriába a változókat
 		with open('debug/E.ssv', 'a+') as efile:
 			line = efile.readline()
-			if line != '' and SEPARATOR in line:
-				unameE, hetE, napE, tantE, anyagE = line.strip().split(SEPARATOR)
+			if line != '' and separatorChar in line:
+				unameE, hetE, napE, tantE, anyagE = line.strip().split(separatorChar)
 
 		with open('debug/J.ssv', 'a+') as jfile:
 			line = jfile.readline()
-			if line != '' and SEPARATOR in line:
-				unameJ, hetJ, napJ, tantJ, anyagJ = line.strip().split(SEPARATOR)
+			if line != '' and separatorChar in line:
+				unameJ, hetJ, napJ, tantJ, anyagJ = line.strip().split(separatorChar)
 
 		returnJSON = {
 			'E':
@@ -84,6 +101,8 @@ def index():
 		return Response(json.dumps(returnJSON))
 
 if __name__ == '__main__':
+	# Ha bennhagyjuk, leállításig rögzített bejegyzések úrjaindításkor törlődnek
 	filetrunc();
+	# Pozitívum, ha már itt tartunk
 	print('[+] Szerver fut')
 	app.run(host='0.0.0.0')
