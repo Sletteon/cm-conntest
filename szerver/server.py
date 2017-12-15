3733ed71f7d66340c1d45eb03f70880f0eddf892
sletteon <Sletteon@users.noreply.github.com>
2017-12-15 22:11:46 +0100

    szerver start IP-cím print

--- szerver/server.py
+++ szerver/server.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os, logging
+import os, logging, socket
 from flask import Flask, request, Response, json
 from flask_cors import CORS
 
@@ -104,6 +104,6 @@ if __name__ == '__main__':
 	# Ha bennhagyjuk, a rögzített bejegyzések úrjaindításkor törlődnek
 	filetrunc();
 
-	app.run(host='0.0.0.0')
 	# Pozitívum, ha már itt tartunk
-	print('[+] Szerver fut')
+	print('[+] Szerver fut: ' + (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0])
+	app.run(host='0.0.0.0')
