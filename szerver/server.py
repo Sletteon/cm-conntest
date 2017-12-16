c008bbc287be257129f554ba4c62ab3a866c869d
sletteon <Sletteon@users.noreply.github.com>
2017-12-16 15:51:24 +0100

    többjsonos lekérés

--- szerver/server.py
+++ szerver/server.py
@@ -15,15 +15,11 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 # Ezzel választja el a fájlokban az adatokat
 separatorChar = '<|>'
 
-# Töröjle a megnyitott fájlok tartalmát
+# Törölje a megnyitott fájlok tartalmát
 def filetrunc():
-	with open('debug/E.ssv', 'a+') as efile:
+	with open('debug/data.json', 'w') as efile:
 		efile.truncate()
 		efile.close()
-
-	with open('debug/J.ssv', 'a+') as jfile:
-		jfile.truncate()
-		jfile.close()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -33,8 +29,8 @@ def index():
 		gotJSON = request.get_json()
 		# [*] jancsi (jancsi.ip.címe.briós) bejegyzése:
 		print('\n' + '[*] ' + str(gotJSON['uname']) + ' (' + clientIP + ')' + ' bejegyzése: ')
-		# próbálja meg a beküldött adatot (JSON) szétválasztani,
-		# lokális változókat létrehozva
+		# Próbálja meg a beküldött adatot (JSON) szétválasztani,
+		# Lokális változókat létrehozva
 		try:
 			uname = gotJSON['uname']
 			het = gotJSON['het']
@@ -51,59 +47,25 @@ def index():
 		print('--- Tantárgy: ' + str(tant))
 		print('--- Anyag: ' + str(anyag))
 
-		# Adatok mentése a megfelelő fájlba
-		if het == 'E':
-			with open('debug/E.ssv', 'a+') as efile:
-				efile.write(separatorChar.join([uname, het, nap, tant, anyag]) + '\n')
-		if het == 'J':
-			with open('debug/J.ssv', 'a+') as jfile:
-				jfile.write(separatorChar.join([uname, het, nap, tant, anyag]) + '\n')
+		with open('debug/data.json', 'a') as file:
+			json.dump(gotJSON, file)
+			file.write('\n')
+
 		return Response(json.dumps('SUCCESS'), mimetype='application/json')
 	else: #request.method == 'GET'
 		# [*] Hétlekérés: jancsi.ip.címe.túróstáska
-		print('\n' + '[*] Hétlekérés: ' + clientIP)
+		print('\n' + '[*] Anyaglekérés: ' + clientIP)
 
-		# Átmeneti (szörnyű) megoldás: hozzon létre változókat,
-		# ha nincs pl. a jövő heti fájlba írva semmi,
-		# akkor is küldje el a jövő heti fájl kulcsait, '' értékkel
-		unameE, hetE, napE, tantE, anyagE, unameJ, hetJ, napJ, tantJ, anyagJ = '' , '' , '' , '' , '' , '' , '' , '' , '' , ''
-		# Nyissa meg a fájlokat, írja a memóriába a változókat
-		with open('debug/E.ssv', 'a+') as efile:
-			line = efile.readline()
-			if line != '' and separatorChar in line:
-					unameE, hetE, napE, tantE, anyagE = line.strip().split(separatorChar)
-
-		with open('debug/J.ssv', 'a+') as jfile:
-			line = jfile.readline()
-			if line != '' and separatorChar in line:
-					unameJ, hetJ, napJ, tantJ, anyagJ = line.strip().split(separatorChar)
-
-
-		returnJSON = {
-			'E':
-			{
-				'uname': unameE,
-				'het': hetE,
-				'nap': napE,
-				'tant': tantE,
-				'anyag': anyagE
-			},
-			'J':
-			{
-				'uname': unameJ,
-				'het': hetJ,
-				'nap': napJ,
-				'tant': tantJ,
-				'anyag': anyagJ
-			}
-		}
-
-		return Response(json.dumps(returnJSON))
+		# Nyissa meg a fájlt, tartalmát küldje el
+		with open('debug/data.json', 'r') as file:
+			fileList = file.readlines()
+			return Response("\n".join(fileList))
 
 if __name__ == '__main__':
 	# Ha bennhagyjuk, a rögzített bejegyzések úrjaindításkor törlődnek
 	filetrunc();
 
 	# Pozitívum, ha már itt tartunk
+	# Lokális IP-cím lekérése zajlik a szerver fut felirat mellett.
 	print('[+] Szerver fut: ' + (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0])
 	app.run(host='0.0.0.0')
