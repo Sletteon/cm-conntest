4eaff53126fd3594c635b74eb2158de3cbe2b352
sletteon <Sletteon@users.noreply.github.com>
2017-12-30 12:41:22 +0100

    meg lehet adni saját portot, működik online, offline

--- szerver/colorPrint.py
+++ szerver/colorPrint.py
@@ -16,9 +16,15 @@ class colorPrint:
 	def finePrint(self, message):
 		print('\n' + Fore.BLUE + '[-] ' + Style.RESET_ALL + message)
 
+
 	def startPrint(self, IpAddress):
 		print(Fore.GREEN + '[+] ' + Style.RESET_ALL + 'Szerver fut: ' + IpAddress)
 
 	def startPrintNoIP(self):
-		print(Fore.GREEN + '[+] ' + Style.RESET_ALL + 'Szerver fut...')
-		self.warnPrint('Nem lehetett IP-címet megállapítani')
+		print(Fore.YELLOW + '[*] ' + Style.RESET_ALL + 'Szerver fut, de nem lehetett IP-címet megállapítani')
+
+	def lowPortNumberWarn(self):
+		print(Fore.YELLOW + '[*] ' + Style.RESET_ALL + 'Nem javasolt 1000-nél kisebb portszámot megadni\n')
+
+	def notValidPortNumberErr(self):
+		print(Fore.RED + '[&&&] ' + Style.RESET_ALL + 'Portszám csak teljes szám lehet')
--- szerver/server.py
+++ szerver/server.py
@@ -16,7 +16,7 @@
 # [&&&] Végzetes hiba (piros, alig látszik)
 
 # Standard könyvárak
-import os, logging, socket
+import os, logging, socket, sys
 
 # Hozzáadott könyvárak
 from flask import Flask, request, Response
@@ -58,6 +58,10 @@ def index():
 	else:  # request.method == 'GET'
 		return onReceiveReqObj.onReceiveGet(clientIP)
 
+def getlocalIp():
+	return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+
+
 if __name__ == '__main__':
 	# Ha bennhagyjuk, a rögzített bejegyzések úrjaindításkor törlődnek
 	fileIO().filetrunc()
@@ -65,8 +69,28 @@ if __name__ == '__main__':
 	# Lokális IP-cím lekérése zajlik a 'szerver fut' felirat mellett.
 
 	try:
-		colorPrint().startPrint((([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0])
-	except Exception as e:
-		colorPrint().startPrintNoIP()
+		port = sys.argv[1]
+		if int(port)<1000:
+			colorPrint().lowPortNumberWarn()
 
-	app.run(host='0.0.0.0')
+		localIp = getlocalIp()
+
+	except IndexError: # ha nincs portszám megadva
+		try:
+			colorPrint().startPrint(getlocalIp() + ':5000')
+			app.run(host='0.0.0.0')
+		except OSError: # ha nem lehet lokális IP-t lekérni
+			colorPrint().startPrintNoIP()
+			app.run(host='0.0.0.0')
+
+	except ValueError: # ha a felhasználó nem érvényes portszámot adott meg
+		colorPrint().notValidPortNumberErr()
+
+	except OSError:
+		colorPrint().startPrintNoIP()
+		colorPrint().okPrint('Szerver elérhető a %s porton' % (str(port)))
+		app.run(host='0.0.0.0', port=int(port))
+
+	else: # ha meg van adva egy érvényes portszám
+		colorPrint().startPrint('%s:%s' %(localIp, str(port)))
+		app.run(host='0.0.0.0', port=int(port))
