51f93633cde0eecebe28b2dcaad16a2793770daa
sletteon <misi.g126@gmail.com>
2017-12-31 16:23:03 +0100

    crlf->lf, 50perce van travisnak buildelni lni & futtatni a tesztet

--- szerver/autoTestClient.py
+++ szerver/autoTestClient.py
@@ -3,33 +3,34 @@
 import requests
 
 # 1000 hosszú json-t küld be, ami egy realisztikus terhelést törekszik elérni
+print('[*] Beállítás-teszt')
 for i in range(1001):
 	try:
 		r = requests.post('http://46.139.116.9:5000',json={'uname':'TESTNUMBER' + str(i), 'het':'E', 'nap':'P', 'tant':'NagyonHosszúTantárgyÉkezetekÉsEsetlegesEgyébProblémákTesztelésére', 'anyag':'EgyRandomAnyagAmitPárRandomÉkezetesBetűKövet:öüóőúáűíÖÜÓŐÚÁŰÍéÉ\|Ä<äđĐłŁ$ß¤'})
 		r.raise_for_status()
 	except requests.exceptions.HTTPError as httperr:
-		print(httperr)
+		print('<!> ' + str(httperr))
 		exit(1)
 	except requests.exceptions.RequestException as e:
-		print(e)
+		print('<!> ' + str(e))
 		exit(1)
 
 	if not r.json() == 'SUCCESS' and not i == 0 :
-		raise ServerError('[!] Szerver rossz választ adott')
+		raise ServerError('<!> Szerver rossz választ adott')
 	else:
 		if not r.json() == 'SUCCESS':
-			raise ServerError('[!] Hibás beállítás')
-	if r.json() == 'SUCCESS':
-		print(str(i) + '. teszt sikeres')
+			raise ServerError('<!> Hibás beállítás')
 
-print('[*] Vége a beírás-tesztnek, lekérés következik')
-try:
-	r = requests.get('http://46.139.116.9:5000')
-	r.raise_for_status()
-	if not r.text == None and not r.text == '':
-		print('[+] Sikeres lekérés')
-	else:
-		raise ServerError('[!] Üres válasz a lekérésre')
-except requests.exceptions.HTTPError as httperr:
-	print(httperr)
-	exit(1)
+if i == 1000:
+	print('[+] Sikeres beállítás-teszt')
+	print('[*] Lekérés')
+	try:
+		r = requests.get('http://46.139.116.9:5000')
+		r.raise_for_status()
+		if not r.text == None and not r.text == '':
+			print('[+] Sikeres lekérés')
+		else:
+			raise ServerError('<!> Üres válasz a lekérésre')
+	except requests.exceptions.HTTPError as httperr:
+		print('<!> ' + str(httperr))
+		exit(1)
