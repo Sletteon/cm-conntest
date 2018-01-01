1f8b5aa7f93e00b8a65e0cdce62f952c01939503
sletteon <Sletteon@users.noreply.github.com>
2018-01-01 11:55:58 +0100

    nem úgy működig a raise, mint ahogy gondoltam

--- szerver/autoTestClient.py
+++ szerver/autoTestClient.py
@@ -6,7 +6,7 @@ import requests, hashlib
 print('[*] Beállítás-teszt')
 for i in range(1001):
 	try:
-		r = requests.post('http://46.139.116.9:5000',json={'uname':'TEST_NUMBER_' + str(i), 'het':'E', 'nap':'P', 'tant': hashlib.sha1(str(i).encode('utf-8')).hexdigest(), 'anyag': hashlib.md5(str(i).encode('utf-8')).hexdigest()})
+		r = requests.post('http://localhost:5000',json={'uname':'TEST_NUMBER_' + str(i), 'het':'E', 'nap':'P', 'tant': hashlib.sha1(str(i).encode('utf-8')).hexdigest(), 'anyag': hashlib.md5(str(i).encode('utf-8')).hexdigest()})
 		r.raise_for_status()
 	except requests.exceptions.HTTPError as httperr:
 		print('<!> ' + str(httperr))
@@ -16,21 +16,22 @@ for i in range(1001):
 		exit(1)
 
 	if not r.json() == 'SUCCESS' and not i == 0 :
-		raise ServerError('<!> Szerver rossz választ adott')
+		raise EnvironmentError('<!> Szerver rossz választ adott')
+		raise Error
 	else:
 		if not r.json() == 'SUCCESS':
-			raise ServerError('<!> Hibás beállítás')
+			raise EnvironmentError('<!> Hibás beállítás')
 
 if i == 1000:
 	print('[+] Sikeres beállítás-teszt')
 	print('[*] Lekérés')
 	try:
-		r = requests.get('http://46.139.116.9:5000')
+		r = requests.get('http://localhost:5000')
 		r.raise_for_status()
 		if not r.text == None and not r.text == '':
 			print('[+] Sikeres lekérés')
 		else:
-			raise ServerError('<!> Üres válasz a lekérésre')
+			raise EnvironmentError('<!> Üres válasz a lekérésre')
 	except requests.exceptions.HTTPError as httperr:
 		print('<!> ' + str(httperr))
 		exit(1)
