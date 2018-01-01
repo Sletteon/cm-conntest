2450cb3d7099792acb81dc4f33672d639a5c3e63
sletteon <Sletteon@users.noreply.github.com>
2018-01-01 13:39:09 +0100

    olvashatóság

--- szerver/colorPrint.py
+++ szerver/colorPrint.py
@@ -5,26 +5,26 @@ from colorama import Fore, Style
 
 class colorPrint:
 	def errPrint(self, message):
-		print('\n' + Fore.RED + '[&&&] ' + Style.RESET_ALL + message)
+		print('\n' + Fore.RED + Style.BRIGHT + '[&&&] ' + Style.RESET_ALL + message)
 
 	def warnPrint(self, message):
-		print('\n' + Fore.YELLOW + '[*] ' + Style.RESET_ALL + message)
+		print('\n' + Fore.YELLOW + Style.BRIGHT + '[*] ' + Style.RESET_ALL + message)
 
 	def okPrint(self, message):
-		print('\n' + Fore.GREEN + '[+] ' + Style.RESET_ALL + message)
+		print('\n' + Fore.GREEN + Style.BRIGHT + '[+] ' + Style.RESET_ALL + message)
 
 	def finePrint(self, message):
-		print('\n' + Fore.BLUE + '[-] ' + Style.RESET_ALL + message)
+		print('\n' + Fore.BLUE + Style.BRIGHT + '[-] ' + Style.RESET_ALL + message)
 
 
 	def startPrint(self, IpAddress):
-		print(Fore.GREEN + '[+] ' + Style.RESET_ALL + 'Szerver fut: ' + IpAddress)
+		print(Fore.GREEN + Style.BRIGHT + '[+] ' + Style.RESET_ALL + 'Szerver fut: ' + IpAddress)
 
 	def startPrintNoIP(self):
-		print(Fore.YELLOW + '[*] ' + Style.RESET_ALL + 'Szerver fut, de nem lehetett IP-címet megállapítani')
+		print(Fore.YELLOW + Style.BRIGHT + '[*] ' + Style.RESET_ALL + 'Szerver fut, de nem lehetett IP-címet megállapítani')
 
 	def lowPortNumberWarn(self):
-		print(Fore.YELLOW + '[*] ' + Style.RESET_ALL + 'Nem javasolt 1000-nél kisebb portszámot megadni\n')
+		print(Fore.YELLOW + Style.BRIGHT + '[*] ' + Style.RESET_ALL + 'Nem javasolt 1000-nél kisebb portszámot megadni\n')
 
 	def notValidPortNumberErr(self):
-		print(Fore.RED + '[&&&] ' + Style.RESET_ALL + 'Portszám csak teljes szám lehet')
+		print(Fore.RED + Style.BRIGHT + '[&&&] ' + Style.RESET_ALL + 'Portszám csak teljes szám lehet')
