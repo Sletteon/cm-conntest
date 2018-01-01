f7fd6f99916dd3e898c8e5f58be6253052781b9b
sletteon <Sletteon@users.noreply.github.com>
2018-01-01 11:56:54 +0100

    az ip címet meg úgyhagytam

--- szerver/autoTestClient.py
+++ szerver/autoTestClient.py
@@ -6,7 +6,7 @@ import requests, hashlib
 print('[*] Beállítás-teszt')
 for i in range(1001):
 	try:
-		r = requests.post('http://localhost:5000',json={'uname':'TEST_NUMBER_' + str(i), 'het':'E', 'nap':'P', 'tant': hashlib.sha1(str(i).encode('utf-8')).hexdigest(), 'anyag': hashlib.md5(str(i).encode('utf-8')).hexdigest()})
+		r = requests.post('http://46.139.116.9:5000',json={'uname':'TEST_NUMBER_' + str(i), 'het':'E', 'nap':'P', 'tant': hashlib.sha1(str(i).encode('utf-8')).hexdigest(), 'anyag': hashlib.md5(str(i).encode('utf-8')).hexdigest()})
 		r.raise_for_status()
 	except requests.exceptions.HTTPError as httperr:
 		print('<!> ' + str(httperr))
@@ -26,7 +26,7 @@ if i == 1000:
 	print('[+] Sikeres beállítás-teszt')
 	print('[*] Lekérés')
 	try:
-		r = requests.get('http://localhost:5000')
+		r = requests.get('http://46.139.116.9:5000')
 		r.raise_for_status()
 		if not r.text == None and not r.text == '':
 			print('[+] Sikeres lekérés')
