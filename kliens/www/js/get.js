9f6b8b130ebe479dae559a2212504826262c6b60
Papp Mihály <papp.misi@protonmail.com>
2018-07-02 06:18:46 +0200

    csak az aktuális hetet kérje le

--- kliens/www/js/get.js
+++ kliens/www/js/get.js
@@ -1,16 +1,13 @@
 var got_to_day = false;
 var locat = window.location.href;
 var AnyagResponse;
-setInterval(function() {
-        if (/nap.html/.test(locat) && got_to_day == false) {
-            got_to_day = true;
-            AnyagLekeres2();
-        }
-    },
-    1000);
+
 window.onload = function() {
     if (/index.html/.test(locat) && got_to_day == false) {
-        AnyagLekeres();
+        ezAHetLekerese();
+    }
+    if (/egy_nap.html/.test(locat) && got_to_day == false) {
+        egyNapAnyagai();
     }
 }
 
@@ -25,19 +22,22 @@ function error() {
     errAlert.innerHTML = '<div class="alert alert-sm alert-danger">Hiba történt a kapcsolat közben.</div>';
     document.getElementById('statusDiv').appendChild(errAlert);
 }
-function getUrl /*hardCoded*/ () {
-    var IPaddress = '46.139.116.9'; /*document.getElementById("IP").value;*/
-    /*var IPaddress = '46.139.116.9';*/
-    var Port = 5000; /*document.getElementById("Port").value;*/
 
-    return "http://" + IPaddress + ":" + Port;
+function getWeek() {
+    // Hét száma az évben
+    Date.prototype.getWeekNumber = function() {
+        var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()))
+        var dayNum = d.getUTCDay() || 7;
+        d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+        var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+        return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
+    };
+    return new Date().getWeekNumber();
 }
 
-function getUrls /*SoftCoded*/ () {
-    var IPaddress = document.getElementById("IP").value;
-    /*var IPaddress = "46.139.116.9";*/
-    var Port = document.getElementById("Port").value;
-
+function getUrl() {
+    var IPaddress = '46.139.116.9';
+    var Port = 5000;
     return "http://" + IPaddress + ":" + Port;
 }
 
@@ -49,6 +49,52 @@ function getUrls /*SoftCoded*/ () {
 Array.prototype.unique = function() {
     return this.filter(function(value, index, self) {
         return self.indexOf(value) === index;
+    });
+}
+
+function ezAHetLekerese() {
+    $.ajax({
+        type: "get",
+        url: getUrl() + '/het/' + getWeek(),
+        success: function(responseData, textStatus, jqXHR) {
+            responseJSON = JSON.parse(responseData)
+            // Összes JSON anyaga
+            pufferAnyag = []
+            for (i = 0; i < responseJSON.length; i++) {
+                pufferAnyag.push(responseJSON[i].anyag);
+            }
+            console.log('Összes JSON anyaga: ' + '\n' + pufferAnyag + '\n -----');
+            /*document.getElementById("socket").innerHTML += '<h2>' + pufferAnyag + '<h2>';*/
+
+            // Összes JSON napja
+            pufferNap = []
+            for (i = 0; i < responseJSON.length; i++) {
+                pufferNap.push(responseJSON[i].nap);
+            }
+            napok = pufferNap.unique()
+            napok.sort()
+            DayDict = {
+                0: "Hétfő",
+                1: "Kedd",
+                2: "Szerda",
+                3: "Csütörtök",
+                4: "Péntek",
+                5: "Szombat",
+                6: "Vasárnap"
+            }
+            var adottNap
+            for (i = 0; i < napok.length; i++) {
+                adottNap = napok[i];
+                document.getElementById('socket').innerHTML += '<button class="btn btn-primary teljesKepernyoBootstrapPrimary" onclick=navigate(' + adottNap + ')>' + DayDict[adottNap] + '</button><br><br>'
+            }
+            // Ne az összes napot írja ki, hanem mindenből csak egyet (ha van legalább arra a napra bejegyzés)
+            console.log('Összes JSON napja: ' + '\n' + pufferNap.unique() + '\n -----');
+
+            console.log(JSON.stringify(responseJSON))
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            error();
+        }
     });
 }
 
@@ -115,8 +161,6 @@ function AnyagLekeres() {
 var clickedButton;
 var url = new URL(window.location.href.replace("index.html", "egy_nap.html")); //URL query param
 
-console.log(clickedButton);
-
 function navigate(x) {
     console.log(clickedButton);
     url.searchParams.append('day', x); //URL query param
@@ -124,7 +168,7 @@ function navigate(x) {
 
 }
 
-function AnyagLekeres2() {
+function egyNapAnyagai() {
     var param = new URLSearchParams(window.location.search);
     clickedButton = param.get('day');
     $.ajax({
@@ -137,7 +181,7 @@ function AnyagLekeres2() {
                 if (responseJSON[i].nap == clickedButton) {
                     /*console.log(clickedButton);*/
                     console.log(responseJSON[i].nap);
-                    document.getElementById("socket2").innerHTML += '<div class="well" id="' + i + '"></div>'
+                    document.getElementById("socket").innerHTML += '<div class="well" id="' + i + '"></div>'
                     document.getElementById(i).innerHTML += '<h2>Tantárgy: ' + responseJSON[i].tant + '</h2>'
                     document.getElementById(i).innerHTML += '<h2>Anyag: ' + responseJSON[i].anyag + '</h2>'
                 }
@@ -145,15 +189,7 @@ function AnyagLekeres2() {
             console.log(JSON.stringify(responseJSON))
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            alert("Hiba a kapcsolat létesítésekor. (lásd konzol)");
+            error();
         }
     });
 }
-
-
-
-// ha kiválasztottak egy kép-fájlt
-
-/*document.getElementById("getButton2").onclick = function() {
-    AnyagLekeres();
-};*/
