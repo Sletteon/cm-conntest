31ea91dfb51bfe0d198001bc87c749d8093b1355
Papp Mihály <papp.misi@protonmail.com>
2018-11-11 08:24:32 +0100

    felhasználónév lekérése fix

--- kliens/www/js/uname_check.js
+++ kliens/www/js/uname_check.js
@@ -1,17 +1,9 @@
 // UNAME_CHECK.JS - felhasználónév ellenőrzése, felhasználónév hiányában gyorsan kérünk egyet
-
-window.onload = function() {
+var UName = window.localStorage.getItem("UName");
+// amennyiben nincs felhnév mentve, prompttal kérjen egyet
+if (UName === null) {
+    UName = prompt("Add meg a beceneved:");
+    window.localStorage.setItem("UName", UName);
     var UName = window.localStorage.getItem("UName");
-    // amennyiben nincs felhnév mentve, prompttal kérjen egyet
-    if (UName == null) {
-        UName = prompt("Add meg a beceneved:");
-        window.localStorage.setItem("UName", UName);
-        var UName = window.localStorage.getItem("UName");
-        success('Új beceneved: ' + UName + '.');
-    }
-
-    if (navigator.onLine == false) {
-        noInternetMessage();
-    }
-
+    success('Új beceneved: ' + UName + '.', true);
 }
